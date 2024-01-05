4218bcaa215fce3fb452ddeb76fb23ea59b105ba
moecly <1349269087ABC@gmail.com>
2024-01-05 16:30:10 +0800

    美化界面

--- main.py
+++ main.py
@@ -31,29 +31,43 @@ class main_window(QWidget):
     def __init__(self):
         super(main_window, self).__init__()
         # self.setGeometry(100, 100, 400, 200)
-        self.setFixedSize(400, 300);
+        self.setFixedSize(700, 500);
         # 主layout
         self.h_box_layout = QHBoxLayout()
         self.main_layout = QVBoxLayout()
         self.main_layout.addLayout(self.h_box_layout)
         self.setLayout(self.main_layout)
 
+        # 设置窗口标题
+        self.setWindowTitle("DSDFU")
+
         # 存储driver
         self.drivers = []
 
-
         # 创建 QFont 对象并设置字体大小
-        programmer_push_button_font = QFont()
-        programmer_push_button_font.setPointSize(26)  # 设置字体大小为16点
+        push_button_font = QFont()
+        push_button_font.setPointSize(26)  # 设置字体大小为16点
         # 烧录按钮
         self.programmer_push_button = QPushButton()
         self.programmer_push_button.setText("programmer")
-        self.programmer_push_button.setFont(programmer_push_button_font)
+        self.programmer_push_button.setFont(push_button_font)
         self.programmer_push_button.clicked.connect(self.on_programmer_push_button_clicked)
         self.main_layout.addWidget(self.programmer_push_button)
 
+        # 清空按钮
+        self.clear_push_button = QPushButton()
+        self.clear_push_button.setText("clear")
+        self.clear_push_button.setFont(push_button_font)
+        self.clear_push_button.clicked.connect(self.on_clear_push_button_clicked)
+        self.main_layout.addWidget(self.clear_push_button)
+
+        # 创建 QFont 对象并设置字体大小
+        listwidget_font = QFont()
+        listwidget_font.setPointSize(16)  # 设置字体大小为16点
+
         # listwidget显示识别到的设备
         self.rec_dev_listwidget = QListWidget()
+        self.rec_dev_listwidget.setFont(listwidget_font)
         self.h_box_layout.addWidget(self.rec_dev_listwidget)
 
         # label显示状态
@@ -71,6 +85,7 @@ class main_window(QWidget):
 
         # listwidget显示烧录成功的设备
         self.writer_success_dev_listwidget = QListWidget()
+        self.writer_success_dev_listwidget.setFont(listwidget_font)
         self.h_box_layout.addWidget(self.writer_success_dev_listwidget)
         
         self.monitor_usb_thread = monitor_usb_device()
@@ -90,6 +105,10 @@ class main_window(QWidget):
             self.rec_dev_listwidget.addItem(item)
             self.curr_dev_num = self.curr_dev_num + 1
             self.status_label.setText(str(self.curr_dev_num))
+    
+    def on_clear_push_button_clicked(self):
+        self.rec_dev_listwidget.clear()
+        self.writer_success_dev_listwidget.clear()
 
     def write_to_device_in_thread(self, drive_letter):
         dfu.write_to_device(drive_letter)
